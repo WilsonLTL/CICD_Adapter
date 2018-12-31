adf7fe537d81020b22a7b8a674d9006b92891922
Wilson lo <wilsonlo1997@gmail.com>
2018-12-31 10:49:23 +0800

    Initial commit

--- Main.py
+++ Main.py
@@ -28,5 +28,5 @@ def alert_update():
 
 if __name__ == '__main__':
     # app.run(host="127.0.0.1", port=5000)
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)
     # app.run(host="172.31.28.201",port=8080)
